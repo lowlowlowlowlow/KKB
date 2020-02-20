0f945f856bab0ec8eb0c2cd77a7adabd81b6918c
lowlowlowlowlow <547295929@qq.com>
2020-02-20 15:41:25 +0800

    react2

--- REACT/previews/2/jsx/src/index.js
+++ REACT/previews/2/jsx/src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from "react-dom";
 import styles from "./index.module.css";
 import logo from "./logo192.png";
 
-//基本使用
+//基本用法
 const name = "React";
 
-//函数使用 {formatName(obj)}
+//函数用法     <div>{formatName(obj)}</div>
 const obj = {
   fistName: "Harry",
   lastName: "Potter"
@@ -16,29 +16,28 @@ function formatName(name) {
   return name.fistName + " " + name.lastName;
 }
 
-//jsx    {greet} 
+//JSX用法
 const greet = <div>good</div>;
 
-//条件语句     {show ? greet : "登录"}   {show && greet}
+//条件语句     {show ? greet : "登录"}        {show && greet}
 const show = true;
 
 //数组
 const a = [0, 1, 2];
 /*
+
     <ul>
-      // diff时候，首先比较type，然后是key，所以同级同类型元素，key值必须得 唯一 
+      // diff时候，首先比较type，然后是key，所以同级同类型元素，key值必须得 唯一
       {a.map(item => (
         <li key={item}>{item}</li>
       ))}
     </ul>
 */
 
-//属性className 不是class
-//style:{{width:100px,height:100px}}
-
+//属性className而不是class    style:{{width:100px,height:100px}}  style用obj
 const jsx = (
   <div className={styles.app}>
-    <div>hello, {name}</div>   
+    <div>hello, {name}</div>
     <div>{formatName(obj)}</div>
     {greet}
     {show ? greet : "登录"}
@@ -66,4 +65,4 @@ ReactDOM.render(jsx, document.getElementById("root"));
 //条件语句
 //数组
 //属性
-//模块化 引入styles.css  使用：className={styles.logo}
+//模块化  引入styles模块  使用      className={styles.logo}
